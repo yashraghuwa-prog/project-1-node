08ef58876c4326156d4c7ac5dad37daad52a19b5
Yash Raghuwanshi <yashraghuwa@gmail.com>
2026-07-23 01:55:14 +0530

    comiit

--- index.js
+++ index.js
@@ -3,16 +3,36 @@ const users=require("./MOCK_DATA.json");
 const app=express()
 const port=8000;
 
-app.get("/api/users",(req,res)=>{
-   return res.json(users);
-});
-
 app.get("/users",(req,res)=>{
     const html=`
     <ul>
-    ${users.map(user=>`<li>${user.first_name}</li>`)}
+    ${users.map(user=>`<li>${user.first_name}</li>`).join("")}
     </ul>`;
     res.send(html);
-})
+});
+
+app.get("/api/users",(req,res)=>{
+   return res.json(users);
+});
+app.route("/api/users/:id").get((req,res)=>{
+    const id=Number(req.params.id);
+    const user=users.find((user)=>user.id===id);
+    return res.json(user);})
+    .patch((req,res)=>{
+        return res.json({status:"pending"})
+    }).delete((req,res)=>{
+        return res.json({status:"pending"})
+    });
+
+
+
+
+
+app.post("/api/users",(req,res)=>{
+    return res.json({status:"pending"});
+});
+
+
+
 app.listen(port ,()=>console.log(`server started at port:${port}`));
 
